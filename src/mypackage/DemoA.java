d85e6d1dbc6db917df781f97cbc853f211650d22
Chaya9 <chaya.pr@gmail.com>
2016-08-06 18:38:39 +0530

    test

---
+++ src/mypackage/DemoA.java
@@ -0,0 +1,20 @@
+//Day11   6/8/2016
+//This maven project is associated with git
+
+package mypackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	@Test
+	public void testA(){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		System.out.println("Maven-Jenkins-test");
+		driver.close();
+		
+	}
+}
+
